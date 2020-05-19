a38bdaee005d2a47736a783c89c8e561060757fa
w2cluster99@iiht.tech <w2cluster99@iiht.tech>
2020-05-19 18:48:31 +0530

    change

---
+++ src/main/java/com/example/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.HelloWorld;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+}
+
